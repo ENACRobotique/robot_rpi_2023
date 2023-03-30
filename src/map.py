72a6334e2753ceb0560b3b141bce880f4e4b7638
BOTXavier <xavier.dartevel@gmail.com>
2023-03-30 17:03:16 +0200

    map_graph qui fonctionne

--- src/map.py
+++ src/map.py
@@ -1,16 +1,20 @@
+import matplotlib.pyplot as plt
+
 class Graph(object):
     
     def __init__(self):
         """Cree un graphe vide"""
         self.adj = {}
+        self.coords = {}
 
     def __repr__(self):
         """Representation en chaine de caracteres d'un graphe"""
         return '<Graph: {0.adj}>'.format(self)
 
-    def add_node(self, u):
+    def add_node(self, u,x,y):
         """Ajoute un noeud u au graphe"""
         self.adj[u] = []
+        self.coords[u] = [x,y]
 
     def add_edge(self, u, v):
         """Ajoute l'arete (u, v) au graphe"""
@@ -37,14 +41,30 @@ def read_graph(file):
     with open(file) as f:
         for line in f:
             l = line.split()
-            g.add_node((l[0],float(l[1]),float(l[2]))) #nom x y 
+            g.add_node(l[0],float(l[1]),float(l[2])) #nom x y 
             points.append(l[0])
-            neighbours.append(l[3])
+            nom_spliter = l[3].split(',')
+            neighbours.append(nom_spliter)
     for count, x in enumerate(neighbours):
         for y in x:
-            g.add_edge(l[count],y)
+            g.add_edge(points[count],y)
     return g
 
-file = "C:\Etude\Clubrobot\robot_rpi_2023\src\graph.txt"
 
+def print_map(graph):
+    g = graph 
+    plt.figure()
+    for point in g.adj :
+        for voisin in g.adj[point]:
+            print(voisin)
+            x1 = g.coords[point][0]
+            y1 = g.coords[point][1]
+            x2 = g.coords[voisin][0]
+            y2 = g.coords[voisin][1]
+            plt.plot([x1,x2],[y1,y2])
+    plt.show()
+
+
+file = 'src\graph.txt'
 g = read_graph(file)
+print_map(g)
