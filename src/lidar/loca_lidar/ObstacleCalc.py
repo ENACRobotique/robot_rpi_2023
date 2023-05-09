fixing frame robot->table rotation

src/lidar/loca_lidar/ObstacleCalc.py
@@ -5,9 +5,9 @@ from functools import lru_cache
 import numpy as np
 from typing import Tuple, List, Union
 try:
-    from loca_lidar.config import table_x_min, table_x_max, table_y_min, table_y_max
+    from loca_lidar.config import table_x_min, table_x_max, table_y_min, table_y_max, lidar_theta_offset
 except ModuleNotFoundError:
-    from config import table_x_min, table_x_max, table_y_min, table_y_max
+    from config import table_x_min, table_x_max, table_y_min, table_y_max, lidar_theta_offset
 
 
 @dataclass
@@ -29,37 +29,13 @@ class ObstacleCalc():
         # !! theta angle is parallel to the y axis currently
         obstacles = []
         rot_angle = (robot_wrt_table[2] + self.theta_offset)
-        print(robot_wrt_table[0], robot_wrt_table[1], rot_angle)
         for pt in lidar_pts: # May be possible to vectorize with numpy to avoid for loop
             # From Lidar Frame to table frame directly
             pt_wrt_lidar = self.polar_lidar_to_cartesian(pt)
             rot_mat = np.array([[np.cos(rot_angle), -np.sin(rot_angle)], [np.sin(rot_angle), np.cos(rot_angle)]])
             pt_wrt_table = np.dot(rot_mat, pt_wrt_lidar) + np.array([robot_wrt_table[0], robot_wrt_table[1]])
-            #apply rotation
-            #apply translation
-            # self.apply_transform(transf, pt_wrt_lidar)
-            #print("transform of ", pt, " is ", pt_wrt_lidar)
-            #print(pt_wrt_lidar[0], pt_wrt_lidar[1])
-            #rotated_pt = self.rotate(pt_wrt_lidar[0], pt_wrt_lidar[1], rot_angle)
-            #print("true rotated",   rotated_pt)
-            #pt_wrt_table = rotated_pt[0] + robot_wrt_table[0], rotated_pt[1] + robot_wrt_table[1]
+            # print("lidar", pt_wrt_lidar)
             obstacles.append(pt_wrt_table)
-            #print("rotated", pt_wrt_table)
-            """ #TODO : make this thing below work
-            # From Lidar Frame to Robot Frame
-            pt_wrt_lidar = self.polar_lidar_to_cartesian(pt)
-            # create a homogeneous coordinate vector for this lidar point in lidar frame
-            v_C_B = np.array([pt_wrt_lidar[0], pt_wrt_lidar[1], 1])
-            # transform the homogeneous coordinate vector from Lidar Frame to Robot Frame
-            v_C_A = np.dot(self.t_lidar_to_robot, v_C_B)
-            pt_wrt_robot = (v_C_A[0], v_C_A[1])
-            ,
-            #From Robot Frame to Table Frame
-            v_C_B = np.array([pt_wrt_robot[0], pt_wrt_robot[1], 1])
-            v_C_A = np.dot(self.t_robot_to_table(robot_wrt_table), v_C_B)
-            pt_wrt_table = (v_C_A[0], v_C_A[1])
-            obstacles.append(pt_wrt_table)
-            """
 
         return obstacles
 
@@ -125,13 +101,23 @@ class ObstacleCalc():
         return (v_C_A[0], v_C_A[1]) 
 
 if __name__ == "__main__":
-    a = ObstacleCalc(0.0, 0.0, 0.0)
-    a.calc_obstacles_wrt_table((1.84, 0.22, 0.0), 
-                               (
-        (1.79 ,180 + 56.52),
-        (1.76, 282), #expected (0.05, -0.094)
-        (2.03, 250) #expected (0.022, 1.0)
-        )) 
+    for t_int in range(0, 10):
+        theta = 2.9 + t_int/100
+        print(theta)
+        a = ObstacleCalc(0.0, 0.0, theta)
+    #a.calc_obstacles_wrt_table((1.84, 0.22, 0.0), 
+    #                           (
+    #    (1.79 ,180 + 56.52),
+    #    (1.76, 282), #expected (0.05, -0.094)
+    #    (2.03, 250) #expected (0.022, 1.0)
+    #    )) 
+
+        a.calc_obstacles_wrt_table((2.04, 1.098, -0.15), 
+            (
+            (2.07, 291.5),
+            (1.50, 57.0)
+            )
+        ) 
 
 
     