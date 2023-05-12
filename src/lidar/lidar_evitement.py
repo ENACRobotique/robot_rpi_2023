7dfff0f74c462853d09746f4dabd2e9d64ef1389
ENAC-Robotique <robotique.enac@gmail.com>
2023-05-12 17:21:45 +0200

    add smarter detection algorithm inside table

--- src/lidar/lidar_evitement.py
+++ src/lidar/lidar_evitement.py
@@ -6,24 +6,76 @@ import numpy as np
 from math import radians
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..')) # Avoids ModuleNotFoundError when finding generated folder
 import generated.lidar_data_pb2 as lidar_data
+import generated.robot_state_pb2 as robot_data
 
 MIN_RADIUS = 20.0e-2     # Filter points on robot
-SLOW_RADIUS = 60.0e-2   # Warning radius: start sending slow messages
+TOLERANCE = 5.0e-2
+MAX_RADIUS = 1.0
+SLOW_RADIUS = 80.0e-2   # Warning radius: start sending slow messages
 STOP_RADIUS = 40.0e-2   # Panic radius: immediately send stop messages
-
+LIDAR_OFFSET = np.radians(45.12) + np.pi
 
 last_status = 'stop'
+last_position = [0.0, 0.0, 0.0] # (x, y, theta)
 lidar_sub = None
+odom_position_pub = None
 proximity_status_pub = None
 
 
 def compute_distances(topic_name, msg, time):
     global last_status
+    global last_position
     global proximity_status_pub
 
-    pts = np.array(msg.distances)
-    pts_filter = pts[(pts>MIN_RADIUS)] # Removing points closer than MIN_RADIUS
+    distances = np.array(msg.distances)
+    angles = np.radians(np.array(msg.angles))
 
+    # Filter points that are too far or too close to be useful
+    indexes = np.logical_and(distances > MIN_RADIUS, distances <= MAX_RADIUS).nonzero()
+    distances_filter = distances[indexes]
+    angles_filter = angles[indexes] - LIDAR_OFFSET
+
+    # Coordinate transform to remove points outside the table
+    x = distances_filter * np.cos(angles_filter + last_position[2]) + last_position[0]
+    y = distances_filter * np.sin(angles_filter + last_position[2]) + last_position[1]
+    cond_x = np.logical_and(x > TOLERANCE, x < (3.0 - TOLERANCE))
+    cond_y = np.logical_and(y > TOLERANCE, y < (2.0 - TOLERANCE))
+    indexes = np.logical_and(cond_x, cond_y).nonzero()
+    
+    distances_filter = distances_filter[indexes]
+
+
+    msg = lidar_data.Proximity()
+    if len(distances_filter) == 0:
+        current_status = 'ok'
+        msg.status = lidar_data.ProximityStatus.OK
+
+    else:
+        closest = np.min(distances_filter)
+
+        # Check cylinder location and send messages to robot
+        msg.closest_distance = closest
+
+        if closest <= STOP_RADIUS:
+            current_status = 'stop'
+            msg.status = lidar_data.ProximityStatus.STOP
+        elif closest <= SLOW_RADIUS:
+            current_status = 'slow'
+            msg.status = lidar_data.ProximityStatus.WARNING
+        else:
+            current_status = 'ok'
+            msg.status = lidar_data.ProximityStatus.OK
+
+
+    if current_status == last_status:
+        proximity_status_pub.send(msg)
+        print(current_status)
+    
+    last_status = current_status 
+
+
+
+    """
     # Check if there are still point left before applying reduction operation
     if len(pts_filter) > 0:
         closest = np.min(pts_filter)
@@ -46,7 +98,17 @@ def compute_distances(topic_name, msg, time):
             proximity_status_pub.send(msg)
             print(current_status)
         
-        last_status = current_status
+        last_status = current_status 
+    """
+    
+
+def get_position(topic_name, msg, time):
+    global last_position
+
+    last_position[0] = msg.x
+    last_position[1] = msg.y
+    last_position[2] = msg.theta
+
     
 if __name__ == '__main__':
     print('Starting ultimate-fallback collision avoidance')
@@ -55,6 +117,8 @@ if __name__ == '__main__':
     proximity_status_pub = ProtoPublisher("proximity_status", lidar_data.Proximity)
     lidar_sub = ProtoSubscriber("lidar_data", lidar_data.Lidar)
     lidar_sub.set_callback(compute_distances)
+    odom_position_sub = ProtoSubscriber('odom_pos', robot_data.Position)
+    odom_position_sub.set_callback(get_position)
 
     while ecal_core.ok():
         time.sleep(0.01)
